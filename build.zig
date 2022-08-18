da801abb607577af751a389790ba0d271aaa941e
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-18 07:48:49 +0200

    changed project name

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("qamar", "src/main.zig");
+    const exe = b.addExecutable("digisim", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.linkLibC();
