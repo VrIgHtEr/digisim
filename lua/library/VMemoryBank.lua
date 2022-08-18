56895d0a6d9c0afd6a6f9bfd113fa6715bf94e58
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-18 09:54:49 +0200

    added memory bank

---
+++ lua/library/VMemoryBank.lua
@@ -0,0 +1,46 @@
+input '!ce'
+input '!oe'
+input '!we'
+input('a', 20)
+output('d', 7)
+
+High 'VCC'
+Low 'GND'
+
+X74139 'csm'
+wire 'GND.q/csm.!ea'
+wire 'a[0-1]/csm.sa'
+wire 'VCC.q/csm.!eb'
+fan 'VCC.q/csm.sb'
+
+OrBank { 'cso', width = 4 }
+fan '!ce/cso.a'
+wire 'csm.!qa/cso.b'
+
+Sram 'm0'
+wire '!oe/m0.!oe'
+wire '!we/m0.!we'
+wire 'cso.q[0]/m0.!ce'
+wire 'a[2-20]/m0.a'
+wire 'd/m0.d'
+
+Sram 'm1'
+wire '!oe/m1.!oe'
+wire '!we/m1.!we'
+wire 'cso.q[1]/m1.!ce'
+wire 'a[2-20]/m1.a'
+wire 'd/m1.d'
+
+Sram 'm2'
+wire '!oe/m2.!oe'
+wire '!we/m2.!we'
+wire 'cso.q[2]/m2.!ce'
+wire 'a[2-20]/m2.a'
+wire 'd/m2.d'
+
+Sram 'm3'
+wire '!oe/m3.!oe'
+wire '!we/m3.!we'
+wire 'cso.q[3]/m3.!ce'
+wire 'a[2-20]/m3.a'
+wire 'd/m3.d'
