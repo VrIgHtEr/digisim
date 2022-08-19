831737520c04c84d45b4a5c728f4fc53d62a70f3
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-19 10:55:18 +0200

    modified .vimrc.lua

--- .vimrc.lua
+++ .vimrc.lua
@@ -1,14 +1,14 @@
-vim.api.nvim_exec("nnoremap <silent> <leader><leader><leader> :wa<cr>:!zig build run<cr>", true)
+vim.api.nvim_exec('nnoremap <silent> <leader><leader><leader> :wa<cr>:!zig build && zig-out/bin/digisim<cr>', true)
 
-local cleanupau = vim.api.nvim_create_augroup("DIGISIM_VIMRC_CLEANUP", { clear = true })
+local cleanupau = vim.api.nvim_create_augroup('DIGISIM_VIMRC_CLEANUP', { clear = true })
 
 local function cleanup()
-	vim.api.nvim_exec("unmap <leader><leader><leader>", true)
-	vim.api.nvim_del_augroup_by_id(cleanupau)
-	vim.notify("unloaded!")
+    vim.api.nvim_exec('unmap <leader><leader><leader>', true)
+    vim.api.nvim_del_augroup_by_id(cleanupau)
+    vim.notify 'unloaded!'
 end
 
-vim.api.nvim_create_autocmd("DirChangedPre", {
-	callback = cleanup,
-	group = cleanupau,
+vim.api.nvim_create_autocmd('DirChangedPre', {
+    callback = cleanup,
+    group = cleanupau,
 })
