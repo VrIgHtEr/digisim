8fcd2ddb52d5ca25994fe7217207ee77ebc56890
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-19 09:07:31 +0200

    digisim, bugfix

--- lua/library/BufferBank.lua
+++ lua/library/BufferBank.lua
@@ -2,12 +2,23 @@ local w = math.floor(opts.width or 1)
 if w < 1 then
     error 'width < 1'
 end
+local d = math.floor(opts.depth or 1)
+if d < 1 then
+    error 'depth < 1'
+end
 
 input('a', w - 1)
 output('q', w - 1)
 
 for i = 0, w - 1 do
-    Buffer('x' .. i)
-    wire('x' .. i .. '.a/a[' .. i .. ']')
-    wire('x' .. i .. '.q/q[' .. i .. ']')
+    local prev = 'x' .. i .. '_0'
+    Buffer(prev)
+    wire('a[' .. i .. ']/prev.a')
+    for j = 1, d - 1 do
+        local n = 'x' .. i .. '_' .. j
+        Buffer(n)
+        wire(prev .. '.q/' .. n .. '.a')
+        prev = n
+    end
+    wire(prev .. '.q/q[' .. i .. ']')
 end
