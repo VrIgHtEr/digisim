26e05d7c4107a65ed69aa12785d61b5c2ab56f95
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-23 09:28:36 +0200

    added some alu ops

--- lua/library/VAlu.lua
+++ lua/library/VAlu.lua
@@ -15,3 +15,21 @@ wire 'b/add.b'
 wire 'd/add.d'
 wire 'sub/add.sub'
 wire '!oe/add.!oe'
+
+VAluAnd { 'and', width = width }
+wire 'a/and.a'
+wire 'b/and.b'
+wire 'd/and.d'
+wire '!oe/and.!oe'
+
+VAluOr { 'or', width = width }
+wire 'a/or.a'
+wire 'b/or.b'
+wire 'd/or.d'
+wire '!oe/or.!oe'
+
+VAluXor { 'xor', width = width }
+wire 'a/xor.a'
+wire 'b/xor.b'
+wire 'd/xor.d'
+wire '!oe/xor.!oe'
