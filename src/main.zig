5c3c079176d6cfaab21acaa8c72cf79daf9236ce
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-09-19 12:01:23 +0200

    updated main.zig

--- src/main.zig
+++ src/main.zig
@@ -7,17 +7,23 @@ const process = std.process;
 const io = std.io;
 const stdout = &@import("output.zig").stdout;
 const buffer = &@import("output.zig").buf;
+const local: [:0]const u8 = ".";
 
 fn errmain() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
+    var projdir: [:0]const u8 = undefined;
     var args = try process.argsAlloc(allocator);
     defer process.argsFree(allocator, args);
-    if (args.len < 2) return error.MissingProjectDir;
+    if (args.len < 2) {
+        projdir = local;
+    } else {
+        projdir = args[1];
+    }
 
-    var sim = try Digisim.init(allocator, args[1]);
+    var sim = try Digisim.init(allocator, projdir);
     defer sim.deinit();
     try sim.runLuaSetup();
     while (true) {
